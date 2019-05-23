40bfe529934289b2925a21f02d451c7c6abad283
Akshdeep <akshdeep.sharma1@gmail.com>
2019-05-23 03:51:31 -0400

    Added 404.vue to handle 404 errors. Added routing for 404s and resume

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import App from 'App.vue';
+import App from './App.vue';
+import Resume from './components/resume.pdf';
+import pageNotFound from './views/404.vue';
 
 Vue.use(Router);
 
@@ -11,7 +13,17 @@ export default new Router({
         {
             path: '/',
             name: 'app',
-            component: App
+            component: App,
+        },
+        {
+            path: '/resume',
+            name: 'resume',
+            component: Resume
+        },
+        {
+            path: '*',
+            name: '404',
+            component: pageNotFound,
         },
     ],
 });
