edd1b804da9adc18bc13410c6a0771242388d23b
Akshdeep <akshdeep.sharma1@gmail.com>
2019-06-01 03:42:40 -0400

    removed test portrait image. Added final portrait image. Updated PageHeader component to better accomodate this change.

--- README.md
+++ README.md
@@ -4,6 +4,3 @@ A website to display my skills, technologies and projects.
 
 This website was coded using HTML, Sass, and Vuejs.
 This was my first project using Vue.js. If you feel that I can improve my code somehow, feel free to let me know. I always accept criticsm.
-
-### Todo ###
- 1. Get a legit image of myself
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,7 +4,7 @@
       <div id="ph" class="container">
         <h1>Software Developer</h1>
         <h2>I design and code things to make my life a smidge easier.</h2>
-        <img src="@/assets/test.png">
+        <img src="@/assets/portrait.png">
       </div>
     </div>
   </section>
@@ -23,24 +23,20 @@ export default {
   color: black;
   margin-top: -10rem;
   h1 {
+    padding-top: 10rem;
     font-size: 3.5rem;
   }
   h2 {
-    margin-top: 0.5rem;
     font-size: 1.5rem;
   }
   img {
-    margin-top: 3.5rem;
-    border-radius: 50%;
-    height: 200px;
-    width: 200px;
+    margin-top: 0.5rem;
+    height: 365px;
+    width: 225px;
   }
 }
 
 @media screen and (max-width: 769px) {
-  #ph {
-    padding-top: 9rem;
-  }
   h1 {
     line-height: 120%;
   }
